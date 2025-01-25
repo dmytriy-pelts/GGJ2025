025ba857a51633bbaf0d337acbed43f2863957d6
dmytriy-pelts <dmytriy.pelts@sbb.ch>
2025-01-25 13:37:41 +0100

    Add sprites for flies; Create a fly

--- Assets/Scripts/FlyBehaviour.cs
+++ Assets/Scripts/FlyBehaviour.cs
@@ -1,12 +1,23 @@
+using GumFly.ScriptableObjects;
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent (typeof(CircleCollider2D)), RequireComponent(typeof(Rigidbody2D))]
 public class FlyBehaviour : MonoBehaviour
 {
     [SerializeField]
+    private Fly _flyProperty;
+    [SerializeField] 
+    private Transform _wingPivot;
+    [SerializeField] 
+    private float _wingFlapDurationInSec;
+
+    [SerializeField]
     private float _flightRadius = 2.0f;
     [SerializeField]
-    private float _maxFlightHeight = 0.3f;
+    private float _maxFlightHeight;
+    [SerializeField]
+    private float _elevationFrequency;
     [SerializeField]
     private Vector2 _anchorPos = Vector2.zero;
 
@@ -23,11 +34,15 @@ public class FlyBehaviour : MonoBehaviour
     private float _evasionTotalCooldown = 1.0f;
     private float _evasionCooldown = 1.0f;
 
+    private bool _isAlive = true;
+
     void Start()
     {
         _flightDirection = (Random.Range(0, 1) < 0.5f) ? -1 : 1;
         Vector2 initPos = _anchorPos;
         initPos.x = Random.Range(-_flightRadius, _flightRadius);
+
+        StartCoroutine(WingFlapping());
     }
 
     Vector2 velo2D = Vector2.one;
@@ -51,9 +66,14 @@ public class FlyBehaviour : MonoBehaviour
             if(Mathf.Abs(oldPos.x - _anchorPos.x) >= _flightRadius)
             {
                 _flightDirection *= -1;
+
+                Vector3 dir = this.transform.localScale;
+                dir.x = _flightDirection * -1; // NOTE(dmytriy): (* -1) as correction because the model faces left as prefab
+                this.transform.localScale = dir;
             }
 
-            float heighAjustment = Mathf.Cos(time * 2.0f) * 2.0f;
+            float heighAjustment = Mathf.Cos(time * _elevationFrequency) * _maxFlightHeight;
+            Debug.Log(heighAjustment);
             Vector2 target = new Vector2(_anchorPos.x + (_flightDirection * (_flightRadius + 0.2f)), _anchorPos.y + heighAjustment);
             finalPos = Vector2.SmoothDamp(oldPos, target, ref velo2D, 0.5f, _flightSpeed);
         }
@@ -72,6 +92,19 @@ public class FlyBehaviour : MonoBehaviour
         _evadingPos = (Vector2)transform.position + diff;
         _flightDirection = 1;
         _evasionCooldown = _evasionTotalCooldown;
+    }
+
+    private IEnumerator WingFlapping()
+    {
+        while(_isAlive)
+        {
+            yield return new WaitForSeconds(_wingFlapDurationInSec);
+
+            Vector3 wingScale = _wingPivot.localScale;
+            wingScale.y *= -1;
+            _wingPivot.localScale = wingScale;
+
+        }
     }
 
     private void OnDrawGizmos()
