55d7709c863347c24c69023157e7305e4f12281a
dmytriy-pelts <dmytriy.pelts@sbb.ch>
2025-01-25 17:38:07 +0100

    Improve bubble flight path and reduce the amount of parameter used

--- Assets/Scripts/BubbleFlightPath.cs
+++ Assets/Scripts/BubbleFlightPath.cs
@@ -1,38 +1,26 @@
+using Unity.Collections;
 using UnityEngine;
 
 public class BubbleFlightPath : MonoBehaviour
 {
     [SerializeField]
-    private float _speed;
-    [SerializeField]
-    private float _curvature;
-
-    [SerializeField]
-    private float _angle;
-
-    [SerializeField]
     private Vector2 _v0;
-
-    [SerializeField]
-    private float _gravity = 9.81f;
-
     [SerializeField]
     private float _gravityReversal = 0f;
 
-    [SerializeField]
-    private float _initForceX = 20f;
-    [SerializeField]
-    private float _reversalForceY = 0f;
+    private const float _angle = 84.0f;
+    private const float _gravity = 9.81f;
+    private const float _totalTime = 10.0f;
+    private const float _reversalForceY = 0f;
+    private const float _gravityChangeConst = 6.936f;
 
     private Vector2 getPostition(float t)
     {
-        // TODO(dmytriy):   - Increase total time based on _gravityReversal scale
-        //                  - Calculate _initForceX and _reversalForceY based on _gravityReversal
-
+        float ajustedGravity = (_gravityReversal > 0.0f) ? _gravity + _gravityReversal * _gravityChangeConst : _gravity; 
         Vector2 _v0Final = _v0;
-        _v0Final.x -= _gravityReversal * _initForceX;
         float x = (_v0Final.x  * Mathf.Cos(_angle * Mathf.Deg2Rad)) * t;
-        float y = (_v0Final.y * Mathf.Sin(_angle * Mathf.Deg2Rad)) * t - ((_gravity - _gravityReversal * t) * t * t * _reversalForceY) / 2f;
+        float y = (_v0Final.y * Mathf.Sin(_angle * Mathf.Deg2Rad)) * t - ((ajustedGravity - _gravityReversal * t) * t * t * _reversalForceY) / 2f;
+        
         return new Vector2(x, y);
     }
 
@@ -40,12 +28,11 @@ public class BubbleFlightPath : MonoBehaviour
     {
         Gizmos.color = Color.yellow;
         float tStep = 0.2f;
-        float tTotal = 6.0f;
-        int steps = (int)(tTotal / tStep);
+        int steps = (int)(_totalTime / tStep);
         for (int i = 0; i < steps; i++)
         {
-            Vector2 pos = getPostition(i * tStep);
-            Gizmos.DrawSphere(pos, 0.1f);
+            Vector2 pos = getPostition(i * tStep) + (Vector2)this.transform.position;
+            Gizmos.DrawSphere(pos, 10f);
         }
     }
 }
