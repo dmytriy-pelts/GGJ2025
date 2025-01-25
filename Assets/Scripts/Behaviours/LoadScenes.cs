3fb9edc9cc13bbd8f36e7055a39dc73dbb127c60
u222761 <simon.meer@sbb.ch>
2025-01-25 19:58:06 +0100

    Add init scene

---
+++ Assets/Scripts/Behaviours/LoadScenes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GumFly.Behaviours
+{
+    public class LoadScenes : MonoBehaviour
+    {
+        [SerializeField]
+        private string[] _scenes;
+
+        private void Start()
+        {
+            for (int i = 0; i < _scenes.Length; i++)
+            {
+                string scene = _scenes[i];
+
+                if (i == 0)
+                {
+                    SceneManager.LoadScene(scene, LoadSceneMode.Single);
+                }
+                else
+                {
+                    SceneManager.LoadScene(scene, LoadSceneMode.Additive);
+                }
+            }
+        }
+    }
+}
